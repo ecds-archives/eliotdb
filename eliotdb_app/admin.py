22e41c8a886420a82ceced5b36393d7b0a42c4fe
Sara Palmer <sepalme@emory.edu>
2014-08-21 11:28:01 -0400

    update admin site

--- eliotdb_app/admin.py
+++ eliotdb_app/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from eliotdb_app.models import Name, Document, Mention
 
-admin.site.register(Name)
-admin.site.register(Document)
 admin.site.register(Mention)
+
+class NameAdmin(admin.ModelAdmin):
+    list_display = ('tei_id', 'surname', 'forename', 'alt_names', 'birth', 'death', 'has_viaf', 'has_odnb')
+    search_fields = ['tei_id', 'surname', 'alt_names']
+    
+admin.site.register(Name, NameAdmin)
+
+class DocumentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['tei_id', 'eliot_vol', 'eliot_part', 'eliot_period', 'eliot_toc', 'src_title_a', 'src_title_m', 'src_title_j', 'src_date', 'src_pub', 'src_plc', 'src_vol', 'src_pg', 'src_id']}),
+        ('Alternate Sources', {'fields': ['alt1_title_a', 'alt1_title_m', 'alt1_title_j', 'alt1_date', 'alt1_pub', 'alt1_plc', 'alt1_vol', 'alt1_pg', 'alt1_id', 'alt2_title_a', 'alt2_title_m', 'alt2_title_j', 'alt2_date', 'alt2_pub', 'alt2_plc', 'alt2_vol', 'alt2_pg', 'alt2_id', 'alt3_title_a', 'alt3_title_m', 'alt3_title_j', 'alt3_date', 'alt3_pub', 'alt3_plc', 'alt3_vol', 'alt3_pg', 'alt3_id'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Document, DocumentAdmin)
--- eliotdb_app/models.py
+++ eliotdb_app/models.py
@@ -2,70 +2,92 @@ from django.db import models
 
 class Name(models.Model):
     objects = models.Manager()
-    tei_id = models.CharField(max_length=50, unique=True)
-    surname = models.CharField(max_length=100)
-    forename = models.CharField(max_length=100)
-    alt_names = models.CharField(max_length=100) 
-    birth = models.CharField(max_length=20)
-    death = models.CharField(max_length=20)
-    viaf = models.CharField(max_length=100)
-    odnb = models.CharField(max_length=100)
-    seg = models.TextField(max_length=1000) 
-    footnote = models.CharField(max_length=5000)
-    notes = models.TextField(max_length=5000)
+    tei_id = models.CharField(max_length=50, unique=True, verbose_name="Tei Id")
+    surname = models.CharField(max_length=100, blank=True, verbose_name="Last Name")
+    forename = models.CharField(max_length=100, blank=True, verbose_name="First Name(s)")
+    alt_names = models.CharField(max_length=100, null=True, blank=True, verbose_name="Alternative Name(s)") 
+    birth = models.CharField(max_length=20, blank=True)
+    death = models.CharField(max_length=20, blank=True)
+    viaf = models.CharField(max_length=100, null=True, blank=True, verbose_name="VIAF")
+    odnb = models.CharField(max_length=100, blank=True, verbose_name="ODNB")
+    seg = models.TextField(max_length=1000, blank=True, verbose_name="Tagged Segment") 
+    footnote = models.TextField(max_length=5000, blank=True, verbose_name="Complete Footnote")
+    notes = models.TextField(max_length=5000, blank=True, verbose_name="Encoder Notes")
     class Meta:
         db_table = 'names'
+        ordering = ['tei_id']
+    def __unicode__(self):
+        return self.tei_id
+    def has_viaf(self):
+        if self.viaf != None and self.viaf != "":
+            return True
+        else:
+            return False
+    has_viaf.boolean = True
+    has_viaf.short_description = "VIAF"
+    def has_odnb(self):
+        if self.odnb != None and self.odnb != "":
+            return True
+        else:
+            return False
+    has_odnb.boolean = True
+    has_odnb.short_description = "ODNB"
+    
 
 class Document(models.Model):
     objects = models.Manager()
-    tei_id = models.CharField(max_length=200, unique=True)
-    eliot_vol = models.CharField(max_length=200)
-    eliot_part = models.CharField(max_length=200)
-    eliot_period = models.CharField(max_length=200)
-    eliot_toc = models.CharField(max_length=200)
-    src_title_a = models.CharField(max_length=200)
-    src_title_m = models.CharField(max_length=200)
-    src_title_j = models.CharField(max_length=200)
-    src_date = models.CharField(max_length=200)
-    src_pub = models.CharField(max_length=200)
-    src_plc = models.CharField(max_length=200)
-    src_vol = models.CharField(max_length=200)
-    src_pg = models.CharField(max_length=200)
-    src_id = models.CharField(max_length=200)
-    alt1_title_a = models.CharField(max_length=200)
-    alt1_title_m = models.CharField(max_length=200)
-    alt1_title_j = models.CharField(max_length=200)
-    alt1_date = models.CharField(max_length=200)
-    alt1_pub = models.CharField(max_length=200)
-    alt1_plc = models.CharField(max_length=200)
-    alt1_vol = models.CharField(max_length=200)
-    alt1_pg = models.CharField(max_length=200)
-    alt1_id = models.CharField(max_length=200)
-    alt2_title_a = models.CharField(max_length=200)
-    alt2_title_m = models.CharField(max_length=200)
-    alt2_title_j = models.CharField(max_length=200)
-    alt2_date = models.CharField(max_length=200)
-    alt2_pub = models.CharField(max_length=200)
-    alt2_plc = models.CharField(max_length=200)
-    alt2_vol = models.CharField(max_length=200)
-    alt2_pg = models.CharField(max_length=200)
-    alt2_id = models.CharField(max_length=200)
-    alt3_title_a = models.CharField(max_length=200)
-    alt3_title_m = models.CharField(max_length=200)
-    alt3_title_j = models.CharField(max_length=200)
-    alt3_date = models.CharField(max_length=200)
-    alt3_pub = models.CharField(max_length=200)
-    alt3_plc = models.CharField(max_length=200)
-    alt3_vol = models.CharField(max_length=200)
-    alt3_pg = models.CharField(max_length=200)
-    alt3_id = models.CharField(max_length=200)
-    notes = models.TextField(max_length=5000)
+    tei_id = models.CharField(max_length=200, unique=True, verbose_name="Tei Id")
+    eliot_vol = models.CharField(max_length=200, blank=True, verbose_name="Volume")
+    eliot_part = models.CharField(max_length=200, blank=True, verbose_name="Part")
+    eliot_period = models.CharField(max_length=200, blank=True, verbose_name="Period")
+    eliot_toc = models.CharField(max_length=200, blank=True, verbose_name="Table of Contents #")
+    src_title_a = models.CharField(max_length=200, blank=True, verbose_name="Article Title")
+    src_title_m = models.CharField(max_length=200, blank=True, verbose_name="Monograph Title")
+    src_title_j = models.CharField(max_length=200, blank=True, verbose_name="Journal Title")
+    src_date = models.CharField(max_length=200, blank=True, verbose_name="Date")
+    src_pub = models.CharField(max_length=200, blank=True, verbose_name="Publisher")
+    src_plc = models.CharField(max_length=200, blank=True, verbose_name="Pub Place")
+    src_vol = models.CharField(max_length=200, blank=True, verbose_name="Pub Volume")
+    src_pg = models.CharField(max_length=200, blank=True, verbose_name="Pages")
+    src_id = models.CharField(max_length=200, blank=True, verbose_name="Gallup #")
+    alt1_title_a = models.CharField(max_length=200, blank=True, verbose_name="Article Title", help_text="Alt Source 1")
+    alt1_title_m = models.CharField(max_length=200, blank=True, verbose_name="Monograph Title", help_text="Alt Source 1")
+    alt1_title_j = models.CharField(max_length=200, blank=True, verbose_name="Journal Title", help_text="Alt Source 1")
+    alt1_date = models.CharField(max_length=200, blank=True, verbose_name="Date", help_text="Alt Source 1")
+    alt1_pub = models.CharField(max_length=200, blank=True, verbose_name="Publisher", help_text="Alt Source 1")
+    alt1_plc = models.CharField(max_length=200, blank=True, verbose_name="Pub Place", help_text="Alt Source 1")
+    alt1_vol = models.CharField(max_length=200, blank=True, verbose_name="Pub Volume", help_text="Alt Source 1")
+    alt1_pg = models.CharField(max_length=200, blank=True, verbose_name="Pages", help_text="Alt Source 1")
+    alt1_id = models.CharField(max_length=200, blank=True, verbose_name="Gallup #", help_text="Alt Source 1")
+    alt2_title_a = models.CharField(max_length=200, blank=True, verbose_name="Article Title", help_text="Alt Source 2")
+    alt2_title_m = models.CharField(max_length=200, blank=True, verbose_name="Monograph Title", help_text="Alt Source 2")
+    alt2_title_j = models.CharField(max_length=200, blank=True, verbose_name="Journal Title", help_text="Alt Source 2")
+    alt2_date = models.CharField(max_length=200, blank=True, verbose_name="Date", help_text="Alt Source 2")
+    alt2_pub = models.CharField(max_length=200, blank=True, verbose_name="Publisher", help_text="Alt Source 2")
+    alt2_plc = models.CharField(max_length=200, blank=True, verbose_name="Pub Place", help_text="Alt Source 2")
+    alt2_vol = models.CharField(max_length=200, blank=True, verbose_name="Pub Volume", help_text="Alt Source 2")
+    alt2_pg = models.CharField(max_length=200, blank=True, verbose_name="Pages", help_text="Alt Source 2")
+    alt2_id = models.CharField(max_length=200, blank=True, verbose_name="Gallup #", help_text="Alt Source 2")
+    alt3_title_a = models.CharField(max_length=200, blank=True, verbose_name="Article Title", help_text="Alt Source 3")
+    alt3_title_m = models.CharField(max_length=200, blank=True, verbose_name="Monograph Title", help_text="Alt Source 3")
+    alt3_title_j = models.CharField(max_length=200, blank=True, verbose_name="Journal Title", help_text="Alt Source 3")
+    alt3_date = models.CharField(max_length=200, blank=True, verbose_name="Date", help_text="Alt Source 3")
+    alt3_pub = models.CharField(max_length=200, blank=True, verbose_name="Publisher", help_text="Alt Source 3")
+    alt3_plc = models.CharField(max_length=200, blank=True, verbose_name="Pub Place", help_text="Alt Source 3")
+    alt3_vol = models.CharField(max_length=200, blank=True, verbose_name="Pub Volume", help_text="Alt Source 3")
+    alt3_pg = models.CharField(max_length=200, blank=True, verbose_name="Pages", help_text="Alt Source 3")
+    alt3_id = models.CharField(max_length=200, blank=True, verbose_name="Gallup #", help_text="Alt Source 3")
+    notes = models.TextField(max_length=5000, blank=True)
     class Meta:
         db_table = 'documents'
+        ordering = ['tei_id']
+    def __unicode__(self):
+        return self.tei_id
 
 class Mention(models.Model):
-    name = models.ForeignKey(Name)
-    document = models.ForeignKey(Document)
+    objects = models.Manager()
+    name_id = models.ForeignKey(Name, to_field='tei_id')
+    document = models.ForeignKey(Document, to_field='tei_id')
     type = models.CharField(max_length=20)
     class Meta:
         db_table = 'mentions'
